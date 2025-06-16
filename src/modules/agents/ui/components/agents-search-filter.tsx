90771d72283e723a79d750584204e7e33e83d40c
wenprompt <wenprompt@gmail.com>
2025-06-16 17:47:28 +0800

    small fixes

--- src/modules/agents/ui/components/agents-search-filter.tsx
+++ src/modules/agents/ui/components/agents-search-filter.tsx
@@ -13,7 +13,7 @@ export const AgentsSearchFilter = () => {
         className="h-9 bg-white w-[200px] pl-7"
         placeholder="Filter by name"
         value={filters.search}
-        onChange={(e) => setFilters({ ...filters, search: e.target.value })}
+        onChange={(e) => setFilters({ search: e.target.value })}
       />
       <SearchIcon className="size-4 absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground" />
     </div>
