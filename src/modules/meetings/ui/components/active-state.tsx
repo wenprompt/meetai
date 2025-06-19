4d0838d176236dbd7da7da9ffe578892a594782a
wenprompt <wenprompt@gmail.com>
2025-06-19 15:33:03 +0800

    21-meeting-variants

---
+++ src/modules/meetings/ui/components/active-state.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { VideoIcon } from "lucide-react";
+
+import { EmptyState } from "@/components/empty-state";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  meetingId: string;
+}
+
+export const ActiveState = ({ meetingId }: Props) => {
+  return (
+    <div className="bg-white rounded-lg px-4 py-5 flex flex-col gap-y-8 items-center justify-center">
+      <EmptyState
+        title="Meeting is active"
+        description="Meeting will end once all participants have left"
+        image="/upcoming.svg"
+      />
+      <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-2 w-full">
+        <Button asChild className="w-full lg:w-auto">
+          <Link href={`/call/${meetingId}`}>
+            <VideoIcon />
+            Join meeting
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -13,6 +13,10 @@ import { useTRPC } from "@/trpc/client";
 import { MeetingIdViewHeader } from "../components/meeting-id-view-header";
 import { useConfirm } from "@/hooks/use-confirm";
 import { UpdateMeetingDialog } from "../components/update-meeting-dialog";
+import { UpcomingState } from "../components/upcoming-state";
+import { ActiveState } from "../components/active-state";
+import { CancelledState } from "../components/cancelled-state";
+import { ProcessingState } from "../components/processing-state";
 
 interface Props {
   meetingId: string;
@@ -50,6 +54,12 @@ export const MeetingIdView = ({ meetingId }: Props) => {
     await removeMeeting.mutateAsync({ id: meetingId });
   };
 
+  const isActive = data.status === "active";
+  const isCompleted = data.status === "completed";
+  const isProcessing = data.status === "processing";
+  const isUpcoming = data.status === "upcoming";
+  const isCancelled = data.status === "cancelled";
+
   return (
     <>
       <RemoveConfirmation />
@@ -67,7 +77,17 @@ export const MeetingIdView = ({ meetingId }: Props) => {
           }}
           onRemove={handleRemoveMeeting}
         />
-        {JSON.stringify(data)}
+        {isCancelled && <CancelledState />}
+        {isProcessing && <ProcessingState />}
+        {isUpcoming && (
+          <UpcomingState
+            meetingId={meetingId}
+            onCancelMeeting={() => {}}
+            isCancelling={false}
+          />
+        )}
+        {isActive && <ActiveState meetingId={meetingId} />}
+        {isCompleted && <div>Completed</div>}
       </div>
     </>
   );
