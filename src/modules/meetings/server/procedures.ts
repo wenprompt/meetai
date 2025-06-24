8d414cc47be47c38d073564538523b8d88dcc24b
wenprompt <wenprompt@gmail.com>
2025-06-24 16:53:11 +0800

    25-completed-state

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -145,6 +145,7 @@ export const meetingsRouter = createTRPCRouter({
       const [existingMeeting] = await db
         .select({
           ...getTableColumns(meetings),
+          agent: agents,
           duration:
             sql<number>`EXTRACT(EPOCH FROM ("ended_at" - "started_at"))`.as(
               "duration"
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -17,6 +17,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
   meetingId: string;
@@ -87,7 +88,7 @@ export const MeetingIdView = ({ meetingId }: Props) => {
           />
         )}
         {isActive && <ActiveState meetingId={meetingId} />}
-        {isCompleted && <div>Completed</div>}
+        {isCompleted && <CompletedState data={data} />}
       </div>
     </>
   );
